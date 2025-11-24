2c94b67954a06db2ae241bd1fd9211902d844ed4
framedstone <leeweixuan39@gmail.com>
2025-11-24 22:09:16 +0800

    refactor: lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,118 +1,111 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-   {
-     "williamboman/mason-lspconfig.nvim",
-     event = "VeryLazy",
-     config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "lua_ls",
-            "pyright",
-            "rust_analyzer",
-            "clangd",
-            "ts_ls",
-            "html",
-            "cssls",
-            "jsonls",
-            "yamlls",
-            "bashls",
-          },
-          automatic_enable = true,
-        })
-     end,
-   },
-     {
-       "neovim/nvim-lspconfig",
-       event = "VeryLazy",
-        dependencies = { "williamboman/mason-lspconfig.nvim", "Saghen/blink.cmp" },
-       config = function()
-        -- LSP on_attach function
-       local on_attach = function(client, bufnr)
-         local opts = { buffer = bufnr, noremap = true, silent = true }
+	{
+		"williamboman/mason.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer",
+		event = "VeryLazy",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- LSPs
+					"lua_ls",
+					"ts_ls",
+					"sqlls",
+					"jdtls",
+					"marksman",
+					"yamlls",
+					"taplo",
+					"dockerls",
+					-- Linters and Formatters
+					"eslint",
+					"prettier",
+					"checkstyle",
+					"google-java-format",
+					"luacheck",
+					"stylua",
+					"markdownlint",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("mason-lspconfig").setup({
+				automatic_enable = true,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = "VeryLazy",
+		dependencies = { "williamboman/mason-lspconfig.nvim", "Saghen/blink.cmp" },
+		config = function()
+			-- LSP on_attach function
+			local on_attach = function(client, bufnr)
+				-- Format on save
+				if client.server_capabilities.documentFormattingProvider then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end
 
-          -- LSP keymaps
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gf", "<cmd>e <cfile><CR>", opts)
-          vim.keymap.set("n", "gF", "<cmd>e <cfile>:<cWORD><CR>", opts)
+			-- LSP capabilities
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-          -- Format on save
-          if client.server_capabilities.documentFormattingProvider then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end,
-            })
-          end
-       end
-
-        -- Diagnostic keymaps (global)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-
-        -- LSP capabilities
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-        -- Configure LSP servers
-        vim.lsp.config('lua_ls', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-              workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-              },
-            },
-          },
-        })
-        vim.lsp.config('pyright', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('rust_analyzer', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('clangd', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('ts_ls', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('html', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('cssls', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('jsonls', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('yamlls', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-        vim.lsp.config('bashls', {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-     end,
-   },
+			-- Configure LSP servers
+			vim.lsp.config("lua_ls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+					},
+				},
+			})
+			vim.lsp.config("ts_ls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			vim.lsp.config("sqlls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			vim.lsp.config("jdtls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			vim.lsp.config("marksman", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			vim.lsp.config("yamlls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			vim.lsp.config("taplo", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			vim.lsp.config("dockerls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
